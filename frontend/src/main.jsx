54d2535689d28fb5c8847b94aeb61ae9cfefe5f0
Caleb Kira <calebnoborukira@gmail.com>
2025-12-20 15:11:57 -0800

    just made a frontend folder with a template for the react app. Gonna add react router dom and organize the pages of what I want where. Then I need to add a backend to store my writings and access any of my stored work. MERN stack baby

---
+++ frontend/src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+/* css that applies to all the "pages", can add a reset here */
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
