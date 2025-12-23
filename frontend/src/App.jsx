2eb693a63f0501038fab8f9011b0018d72347780
Caleb Kira <calebnoborukira@gmail.com>
2025-12-23 13:54:46 -0800

    made a page with all the links persistent across all the pges in a nav element, added a reset.css, and made a general organization for the folders.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
+import './styles/App.css'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Projects from './pages/Projects.jsx'
+import Contact from './pages/Contact.jsx'
+import Music from './pages/projects/Music.jsx'
+import Games from './pages/projects/Games.jsx'
+import VoiceOver from './pages/projects/VoiceOver.jsx'
+import Writing from './pages/projects/Writing.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/projects">
+                    Projects
+                    <nav>
+                        <Link to="/projects/music">Music</Link>
+                        <Link to="/projects/writing">Writing</Link>
+                        <Link to="/projects/games">Games</Link>
+                        <Link to="/projects/voiceover">Voice Over</Link>
+                    </nav>
+                </Link>
+                <Link to="/contact">Contact</Link>
+            </nav>
+            {/* this is effectively setting a nav element in the highest part
+                of the app so that the nav bar persists always */}
+            
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/about" element={<About />}/>
+                <Route path="/projects" element={<Projects />}/>
+                <Route path="/projects/music" element={<Music />}/>
+                <Route path="/projects/Games" element={<Games />}/>
+                <Route path="/projects/voiceover" element={<VoiceOver />}/>
+                <Route path="/projects/writing" element={<Writing />}/>
+                <Route path="/contact" element={<Contact />}/>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
---
+++ frontend/src/pages/projects/VoiceOver.jsx
@@ -0,0 +1,11 @@
+
+
+function VoiceOver(){
+    return (
+        <>
+            <h1>Voice Over</h1>
+        </>
+    )
+}
+
+export default VoiceOver;
