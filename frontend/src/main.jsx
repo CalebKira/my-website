634370a6953778c98a8cb4ad7944f28e7db9f7fe
Caleb Kira <calebnoborukira@gmail.com>
2026-09-01 13:31:59 -0700

    just cleaned up two lines to make it clearer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './reset.css'
+import './styles/reset.css'
 import './index.css'
 /* css that applies to all the "pages", can add a reset here */
 import App from './App.jsx'
