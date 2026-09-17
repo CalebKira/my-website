7f4124f643b345fc7e3c416ea1baed2d28013f74
Caleb Kira <calebnoborukira@gmail.com>
2026-09-17 15:25:59 -0700

    some edits to allow this to build and be run in github pages

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -6,7 +6,7 @@ function Button({ name, route }){
             to={route}
             className="text-xl inline-block w-fit rounded-md bg-(color:--highlight) px-3 py-2 text-black transition-colors transition-shadow hover:bg-(color:--select-color) hover:text-white hover:shadow-md"
         >
-            {name}
+            {name} 
         </Link>
     )
 }
